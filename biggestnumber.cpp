cce39701ece2fd820b4d5bbf31418c1fbfd2760e
Anthony G Micodin <amicodin@ecc-jaguar.csuchico.edu>
2018-09-10 16:25:31 -0700

    Added if else statement

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -4,19 +4,29 @@
 int main()
 {
   //declare variables here
+  int first;
+  int second;
+  int biggest;
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+  std::cin>>first; 
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+  std::cin>>second;
   //determine if first or second is bigger and store that in a third variable
-
+  if( first > second )
+  {
+          biggest = first;
+  }
+  else
+  {
+          biggest = second;
+  }
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
-
+  std::cout<<biggest<<std::endl;
   std::cout<<std::endl<<"Thank you for playing.\n";
   
   return 0;
